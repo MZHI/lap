64f6a5cad42d25fe92b121c6fa919e5a324ee885
Zhidenko Mikhail <zhidenko.michael@gmail.com>
2024-08-30 10:51:25 +0300

    add setuptools limiting version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if SETUPTOOLS_COMMANDS.intersection(sys.argv):
         include_package_data=True,
         extras_require={
             'alldeps': (
-                'numpy == {0}'.format(NUMPY_MIN_VERSION),
+                'numpy >= {0}'.format(NUMPY_MIN_VERSION),
             ),
         },
     )
@@ -127,7 +127,7 @@ def get_numpy_status():
         import numpy
         numpy_version = numpy.__version__
         numpy_status['up_to_date'] = parse_version(
-            numpy_version) == parse_version(NUMPY_MIN_VERSION)
+            numpy_version) >= parse_version(NUMPY_MIN_VERSION)
         numpy_status['version'] = numpy_version
     except ImportError:
         traceback.print_exc()
@@ -172,8 +172,8 @@ def configuration(parent_package='', top_path=None):
 
 def setup_package():
     metadata = dict(name=DISTNAME,
-                    setup_requires=["numpy>=1.21.6", "cython==0.29.32"],
-                    install_requires=["numpy>=1.21.6", "cython==0.29.32"],
+                    setup_requires=["numpy>=1.21.6", "cython==0.29.32", "setuptools<60.0"],
+                    install_requires=["numpy>=1.21.6", "cython==0.29.32", "setuptools<60.0"],
                     maintainer=MAINTAINER,
                     maintainer_email=MAINTAINER_EMAIL,
                     description=DESCRIPTION,
